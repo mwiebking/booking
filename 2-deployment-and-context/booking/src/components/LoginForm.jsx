5730c230718012332c298e24d6dcc0d2616008fb
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2024-11-18 13:24:21 +0100

    Fra klassen: LoginForm vha. FormData

--- 2-deployment-and-context/booking/src/components/LoginForm.jsx
+++ 2-deployment-and-context/booking/src/components/LoginForm.jsx
@@ -1,6 +1,7 @@
-import { TextInput } from "@mantine/core";
+import { TextInput, PasswordInput, Button } from "@mantine/core";
 import { Container } from "@mantine/core";
 import styles from "./login-form.module.css";
+import { useState } from "react";
 
 export default function LoginForm() {
   const containerProps = {
@@ -8,19 +9,26 @@ export default function LoginForm() {
     mt: "md",
   };
 
+  function handleLogin() {
+    const formData = new FormData(document.querySelector("#login-form"));
+    const email = formData.get("email");
+    const password = formData.get("password");
+    console.log(email);
+    console.log(password);
+  }
+
   return (
     <div>
       <Container className={styles.container} {...containerProps}>
-        <TextInput
-          label="Input label"
-          description="Input description"
-          placeholder="Input placeholder"
-        />
-        <TextInput
-          label="Input label"
-          description="Input description"
-          placeholder="Input placeholder"
-        />
+        <form onSubmit={handleLogin} id="login-form">
+          <TextInput label="Email" placeholder="email@gmail.com" name="email" />
+          <PasswordInput
+            label="Password"
+            placeholder="PasswordErSvært"
+            name="password"
+          />
+          <Button onClick={handleLogin}>Login</Button>
+        </form>
       </Container>
     </div>
   );
