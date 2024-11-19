01fd367e25fab1cbef4c248806b93e1bf6201b01
mwiebking <mikewiebking@gmail.com>
2024-11-19 13:13:21 +0100

    Updates

--- assets/about.lazy-BvLe2mY8.js
+++
@@ -1,1 +0,0 @@
-import{n as t,j as e}from"./index-C7to9-Kl.js";import{u as n}from"./useRouteContext-KlLEzhFY.js";const c=t("/about")({component:r});function r(){const o=n({from:"/about"});return console.log(o),e.jsx("div",{children:"Hello from About!"})}export{c as Route};
--- assets/useRouteContext-KlLEzhFY.js
+++
@@ -1,1 +0,0 @@
-import{w as c}from"./index-C7to9-Kl.js";function o(e){return c({...e,select:t=>e.select?e.select(t.context):t.context})}export{o as u};
