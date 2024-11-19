6851257324f8c28a5d44d4c6674a536fc5cea239
mwiebking <mikewiebking@gmail.com>
2024-11-19 11:42:55 +0100

    Updates

--- assets/index.lazy-DzrLhiZF.js
+++
@@ -1,1 +0,0 @@
-import{s as t,j as e}from"./index-BzFSh0FN.js";import{u as s}from"./useRouteContext-CgT1jSmT.js";const i=t("/")({component:r});function r(){var n;const o=s({from:"/"});return console.log(o),e.jsxs("div",{children:[e.jsx("div",{children:((n=o.userInfo)==null?void 0:n.name)??"No name"}),e.jsx("h3",{children:"Welcome Home!"})]})}export{i as Route};
--- assets/useRouteContext-CgT1jSmT.js
+++
@@ -1,1 +0,0 @@
-import{z as c}from"./index-BzFSh0FN.js";function o(e){return c({...e,select:t=>e.select?e.select(t.context):t.context})}export{o as u};
