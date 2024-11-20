412437d557ffc256068f4c97c0e5d4efd87ae0d8
mwiebking <mikewiebking@gmail.com>
2024-11-20 15:28:17 +0100

    latest updates

--- 2-deployment-and-context/booking/src/routes/dashboard-student.lazy.jsx
+++ 2-deployment-and-context/booking/src/routes/dashboard-student.lazy.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { NavbarNested } from '../components/NavbarNested'
 
 export const Route = createLazyFileRoute('/dashboard-student')({
   component: RouteComponent,
@@ -9,7 +10,6 @@ function RouteComponent() {
   return (
     <div>
       <h1>Welcome to the Student Dashboard</h1>
-      {/* Add other components or features specific to the student dashboard here */}
     </div>
   )
 }
