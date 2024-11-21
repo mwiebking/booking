8f44dd242dc9ca9a9cb2599a5948243f852b3a09
mwiebking <mikewiebking@gmail.com>
2024-11-21 15:00:26 +0100

    Updates

---
+++ assets/auditorium.lazy-D_b-Mc3D.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-Crsb3sVN.js";const u=o("/auditorium")({component:t});function t(){return"Hello /auditorium!"}export{u as Route};
---
+++ assets/makerlab.lazy-DWjUPtCg.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-Crsb3sVN.js";const n=e("/makerlab")({component:o});function o(){return"Hello /makerlab!"}export{n as Route};
