5580d071696a5a2105190b3dda9981720b1f317d
mwiebking <mikewiebking@gmail.com>
2024-12-02 18:06:19 +0100

    Updates

---
+++ assets/useRouteContext-CGY0w-r5.js
@@ -0,0 +1,1 @@
+import{a7 as c}from"./index-DA0SvMbs.js";function o(e){return c({...e,select:t=>e.select?e.select(t.context):t.context})}export{o as u};
--- assets/useRouteContext-CUrpPMyX.js
+++
@@ -1,1 +0,0 @@
-import{a6 as c}from"./index-daX0Mh6q.js";function o(e){return c({...e,select:t=>e.select?e.select(t.context):t.context})}export{o as u};
