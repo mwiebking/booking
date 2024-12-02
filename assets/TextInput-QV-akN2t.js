95f30587c79a59db067eaa4ad4c0b6aca12b3bec
mwiebking <mikewiebking@gmail.com>
2024-12-02 16:43:58 +0100

    Updates

--- assets/TextInput-QV-akN2t.js
+++
@@ -1,1 +0,0 @@
-import{h as n,u as a,j as r}from"./index-BiJi3uwm.js";import{I as t}from"./InputBase-CRZR3YvE.js";const u={},s=n((e,o)=>{const p=a("TextInput",u,e);return r.jsx(t,{component:"input",ref:o,...p,__staticSelector:"TextInput"})});s.classes=t.classes;s.displayName="@mantine/core/TextInput";export{s as T};
