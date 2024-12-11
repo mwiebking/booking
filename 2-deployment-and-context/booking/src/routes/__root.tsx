2989945e07c3229b0d01bc1d4d6199f45d5fa1aa
Anton B. Christensen <webbermanden@gmail.com>
2024-12-11 11:40:51 +0100

    Root_ nav væk

--- 2-deployment-and-context/booking/src/routes/__root.tsx
+++ 2-deployment-and-context/booking/src/routes/__root.tsx
@@ -20,21 +20,6 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 function RootRouteWithLayout() {
   return (
     <>
-      <div>
-        <Link to="/">Home </Link>
-        <Link to="/login">Login </Link>
-        <Link to="/signup">Signup </Link>
-        <Link to="/forgotpassword">Forgot password </Link>
-        <Link to="/dashboard">Dashboard </Link>
-        <Link to="/booklokale">Book lokale </Link>
-        <Link to="/mybookings">Mine bookninger </Link>
-        <Link to="/oversigt">Lokale oversigt </Link>
-        <Link to="/medialab">Medialab </Link>
-        <Link to="/makerlab">Makerlab </Link>
-        <Link to="/auditorium">Auditorium </Link>
-        <Link to="/profilepage">Profilepage</Link>
-      </div>
-      <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
