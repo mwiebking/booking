621be9994d6abc062cb98dc92a7bb430d0e20888
mwiebking <mikewiebking@gmail.com>
2024-12-11 09:54:19 +0100

    Updates

---
+++ assets/TextInput-Bidcm1bv.js
@@ -0,0 +1,1 @@
+import{f as a,a as n,j as r}from"./index-DI4ieUrN.js";import{I as t}from"./InputBase-gJGkVLE-.js";const c={},s=a((e,o)=>{const p=n("TextInput",c,e);return r.jsx(t,{component:"input",ref:o,...p,__staticSelector:"TextInput"})});s.classes=t.classes;s.displayName="@mantine/core/TextInput";export{s as T};
