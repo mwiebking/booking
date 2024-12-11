b0136e8650d6e806d2596465ebf04f5692e6190b
mwiebking <mikewiebking@gmail.com>
2024-12-11 12:34:38 +0100

    manglede anon key, tilføjet

--- 2-deployment-and-context/booking/src/components/LoginForm.jsx
+++ 2-deployment-and-context/booking/src/components/LoginForm.jsx
@@ -17,7 +17,7 @@ export default function LoginForm() {
   const navigate = useNavigate();
 
   const SUPABASE_URL = "https://ixfyejbgmefahxcopxea.supabase.co";
-  const PUBLIC_ANON_KEY = "your-public-anon-key";
+  const PUBLIC_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4ZnllamJnbWVmYWh4Y29weGVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5MzQzMTEsImV4cCI6MjA0NzUxMDMxMX0.C4NV6ZDFxDgrH4RSShCLZXonuLjHg_xsilsuYsMkDPQ";
 
   async function handleLogin(e) {
     e.preventDefault();
