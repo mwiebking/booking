f66503d69c8035cf895b5a1c68182a36a6ad7575
mwiebking <mikewiebking@gmail.com>
2024-12-11 19:59:12 +0100

    latest ver

--- 2-deployment-and-context/booking/src/components/LoginForm.jsx
+++ 2-deployment-and-context/booking/src/components/LoginForm.jsx
@@ -171,6 +171,3 @@ export default function LoginForm() {
 }
 
 
-
-
-const PUBLIC_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4ZnllamJnbWVmYWh4Y29weGVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5MzQzMTEsImV4cCI6MjA0NzUxMDMxMX0.C4NV6ZDFxDgrH4RSShCLZXonuLjHg_xsilsuYsMkDPQ";
